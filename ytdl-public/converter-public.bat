45884ae1c2fcbdbf3fa934a0ce91f4d644dd28b8
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-05-13 19:35:36 -0300

    Create converter-public.bat

---
+++ ytdl-public/converter-public.bat
@@ -0,0 +1,41 @@
+@echo off
+cls
+# THIS WILL CONVERT ALL THE MUSIC FILES INSIDE THIS FOLDER TO .OGG / VORBIS TO THE FOLDER "OGG"; IF "OGG" DOESN'T EXIST, IT WILL BE CREATED; FFMPEG AND THE MUSIC FILES MUST BE WITH THE CONVERTER; CHOOSE THE FILE TYPE YOU WANNA CONVERT TO OGG.
+choice /n /c:123 /m "MP3 [1] // FLAC [2] // WAV [3] // Exit [4]"
+if errorlevel 4 exit
+if errorlevel 3 goto PROMPTwav
+if errorlevel 2 goto PROMPTflac
+if errorlevel 1 goto PROMPTmp3
+
+:PROMPTmp3
+if exist "*.mp3" goto convert
+exit
+
+:PROMPTflac
+if exist "*.flac" goto convert
+exit
+
+:PROMPTYwav
+if exist "*.wav" goto convert
+exit
+
+:convertwav
+if not exist "OGG" mkdir OGG
+for %%a in ("*.wav") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+pause
+%SystemRoot%\explorer.exe "OGG"
+exit
+
+:convertmp3
+if not exist "OGG" mkdir OGG
+for %%a in ("*.flac") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+pause
+%SystemRoot%\explorer.exe "OGG"
+exit
+
+:convertflac
+if not exist "OGG" mkdir OGG
+for %%a in ("*.mp3") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+pause
+%SystemRoot%\explorer.exe "OGG"
+exit
