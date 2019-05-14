6b35d96e2d0ceb9439d642cb989eb6416e9ad0a5
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-05-14 10:43:25 -0300

    Update youtube-dl options

--- ytdl-public/ytdl-public.bat
+++ ytdl-public/ytdl-public.bat
@@ -6,14 +6,14 @@ if errorlevel 2 goto mult
 if errorlevel 1 goto single
 :single
 set /p URL=URL:
-youtube-dl -i --ignore-config %URL%
+youtube-dl --newline --no-mtime -i -o %(title)s.%(ext)s -x --audio-format vorbis -audio-quality 0 --ignore-config %URL%
 %SystemRoot%\explorer.exe %cd%
 exit
 :mult
 if not exist "list.txt" echo # Paste your URLs below with each URL on a line, then save it and go back to the prompt. >> "list.txt"
 start notepad %cd%\list.txt
 pause
-youtube-dl -i --ignore-config -a list.txt
+youtube-dl --newline --no-mtime -i -o %(title)s.%(ext)s -x --audio-format vorbis -audio-quality 0 --ignore-config -a list.txt
 %SystemRoot%\explorer.exe %cd%
 exit
 :end
