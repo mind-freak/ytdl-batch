78073ee668304bb9e6f384451ec21e376840f9c1
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-05-24 10:10:03 -0300

    add m4a and fix goto

--- ytdl-public/converter-public.bat
+++ ytdl-public/converter-public.bat
@@ -5,22 +5,34 @@ echo THIS FOLDER TO .OGG / VORBIS TO THE FOLDER "OGG";
 echo IF "OGG" DOESN'T EXIST, IT WILL BE CREATED;
 echo FFMPEG AND THE MUSIC FILES MUST BE WITH THE CONVERTER;
 echo CHOOSE THE FILE TYPE YOU WANNA CONVERT TO OGG.
-choice /n /c:123 /m "MP3 [1] // FLAC [2] // WAV [3] // Exit [4]"
-if errorlevel 4 exit
+choice /n /c:123 /m "MP3 [1] // FLAC [2] // WAV [3] // M4A [4] // Exit [5]"
+if errorlevel 5 exit
+if errorlevel 4 goto PROMPTm4a
 if errorlevel 3 goto PROMPTwav
 if errorlevel 2 goto PROMPTflac
 if errorlevel 1 goto PROMPTmp3
 
 :PROMPTmp3
-if exist "*.mp3" goto convert
+if exist "*.mp3" goto convertmp3
 exit
 
 :PROMPTflac
-if exist "*.flac" goto convert
+if exist "*.flac" goto convertflac
 exit
 
 :PROMPTYwav
-if exist "*.wav" goto convert
+if exist "*.wav" goto convertwav
+exit
+
+:PROMPTm4a
+if exists "*.m4a" goto convertm4a
+exit
+
+:convertm4a
+if not exist "OGG" mkdir OGG
+for %%a in ("*.m4a) do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+pause
+%SystemRoot%\explorer.exe "OGG"
 exit
 
 :convertwav
