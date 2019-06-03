13c535783806a2a10cf9c1e8f4dd44d150407e3f
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-06-03 14:36:51 -0300

    Update video-public.bat

--- ytdl-public/video-public.bat
+++ ytdl-public/video-public.bat
@@ -3,6 +3,6 @@ set /p URL=URL:
 youtube-dl --ignore-config -F %URL%
 set /p FORMAT=FORMAT:
 youtube-dl --no-mtime -i -o %%(title)s.%%(ext)s -f %FORMAT% --ignore-config %URL%
-# %SystemRoot%\explorer.exe %cd%
+start %cd%
 pause
 exit
