3a8444a3e1a2a9e051363149230bba5c8c8f0950
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-06-22 14:03:17 -0300

    Update video-public.bat

--- ytdl-public/video-public.bat
+++ ytdl-public/video-public.bat
@@ -1,7 +1,10 @@
 @echo off
 set /p URL=URL:
 youtube-dl --ignore-config -F %URL%
-set /p FORMAT=FORMAT CODE:
+set /p FORMAT=FORMAT:
 youtube-dl --no-mtime -i -o %%(title)s.%%(ext)s -f %FORMAT% --ignore-config %URL%
-start %cd%
+:opendir
+choice /n /c:yn /m "Open directory [Y/N]"
+if errorlevel 2 exit
+if errorlevel 1 start %cd%
 exit
