2f793b2eecbc9900b971cef66792ed768efeda51
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-06-21 22:54:41 -0300

    Update video-public.bat

--- ytdl-public/video-public.bat
+++ ytdl-public/video-public.bat
@@ -1,8 +1,7 @@
-%echo off
+@echo off
 set /p URL=URL:
 youtube-dl --ignore-config -F %URL%
-set /p FORMAT=FORMAT:
+set /p FORMAT=FORMAT CODE:
 youtube-dl --no-mtime -i -o %%(title)s.%%(ext)s -f %FORMAT% --ignore-config %URL%
 start %cd%
-pause
 exit
