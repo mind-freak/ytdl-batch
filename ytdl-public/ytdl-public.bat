0b8cafc42a3996912ef473e8c7e6f793b2d5a5ed
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-06-22 13:53:40 -0300

    bug fix and update

--- ytdl-public/ytdl-public.bat
+++ ytdl-public/ytdl-public.bat
@@ -5,13 +5,19 @@ if errorlevel 2 goto mult
 if errorlevel 1 goto sing
 :sing
 set /p URL=URL:
-youtube-dl --newline --no-mtime -i -o %%(title)s.%%(ext)s -x --audio-format vorbis -audio-quality 0 --ignore-config %URL%
-start %cd%
-exit
+youtube-dl --newline --no-mtime -i -o "%%(title)s.%%(ext)s" -x --audio-format vorbis --audio-quality 0 --ignore-config %URL%
+goto opendir
+
 :mult
 if not exist "list.txt" echo # Paste your URLs below with each URL on a line, then save it and go back to the prompt. >> "list.txt"
-start notepad %cd%\list.txt
-pause
-youtube-dl --newline --no-mtime -i -o %%(title)s.%%(ext)s -x --audio-format vorbis -audio-quality 0 --ignore-config -a list.txt
-start %cd%
+start %cd%\list.txt
+choice /n /c:yn /m "Continue [Y/N]"
+if errorlevel 2 exit
+if errorlevel 1 youtube-dl --newline --no-mtime -i -o "%%(title)s.%%(ext)s" -x --audio-format vorbis --audio-quality 0 --ignore-config -a list.txt
+goto opendir
+
+:opendir
+choice /n /c:yn /m "Open directory [Y/N]"
+if errorlevel 2 exit
+if errorlevel 1 start %cd%
 exit
