241771b0698069ef6b7ab22a5ddf4356a09c4e3c
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-07-02 05:10:17 -0300

    Update converter-public.bat

--- ytdl-public/converter-public.bat
+++ ytdl-public/converter-public.bat
@@ -11,23 +11,18 @@ if errorlevel 4 goto PROMPTm4a
 if errorlevel 3 goto PROMPTwav
 if errorlevel 2 goto PROMPTflac
 if errorlevel 1 goto PROMPTmp3
-
 :PROMPTmp3
 if exist "*.mp3" goto convertmp3
 goto 404
-
 :PROMPTflac
 if exist "*.flac" goto convertflac
 goto 404
-
 :PROMPTwav
 if exist "*.wav" goto convertwav
 goto 404
-
 :PROMPTm4a
 if exist "*.m4a" goto convertm4a
 goto 404
-
 :PROMPTothr
 set /p FORMAT=FORMAT (without the dot):
 if exist "*.%FORMAT%" goto convertothr
@@ -37,25 +32,21 @@ goto 404
 if not exist "OGG" mkdir OGG
 for %%a in ("*.%FORMAT%) do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
 goto opendir
-
 :convertm4a
 if not exist "OGG" mkdir OGG
 for %%a in ("*.m4a) do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
 goto opendir
-
 :convertwav
 if not exist "OGG" mkdir OGG
 for %%a in ("*.wav") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
 goto opendir
-
 :convertmp3
 if not exist "OGG" mkdir OGG
-for %%a in ("*.flac") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+for %%a in ("*.mp3") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
 goto opendir
-
 :convertflac
 if not exist "OGG" mkdir OGG
-for %%a in ("*.mp3") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+for %%a in ("*.flac") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
 goto opendir
 
 :opendir
