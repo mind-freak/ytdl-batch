26908aa8d809ff00c405769ebbbb6cd858c49c99
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-07-02 05:13:06 -0300

    added "another process"

--- ytdl-public/video-public.bat
+++ ytdl-public/video-public.bat
@@ -1,8 +1,12 @@
 @echo off
+:vid
 set /p URL=URL:
 youtube-dl --ignore-config -F %URL%
 set /p FORMAT=FORMAT CODE:
 youtube-dl --no-mtime -i -o "%%(title)s.%%(ext)s" -f %FORMAT% --ignore-config %URL%
+choice /n /c:yn /m "Another process [Y/N]"
+if errorlevel 2 goto opendir
+if errorlevel 1 goto vid
 :opendir
 choice /n /c:yn /m "Open directory [Y/N]"
 if errorlevel 2 exit
