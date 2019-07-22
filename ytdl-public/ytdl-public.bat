021398c601ee471c2d24591a2bcc4378a4dd236c
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-07-22 10:20:04 -0300

    Added Format changing

--- ytdl-public/ytdl-public.bat
+++ ytdl-public/ytdl-public.bat
@@ -1,11 +1,14 @@
 @echo off
-choice /n /c:123 /m "Single Url [1] // Multiple Urls [2] // Exit [3]"
-if errorlevel 3 goto exit
+:start
+for /f "delims= skip=1" %%x in (config.txt) do (set "%%x")
+choice /n /c:1234 /m "Single Url [1] // Multiple Urls [2] // Change Default Format [3] // Exit [4]"
+if errorlevel 4 goto exit
+if errorlevel 3 goto form
 if errorlevel 2 goto mult
 if errorlevel 1 goto sing
 :sing
 set /p URL=URL:
-youtube-dl --newline --no-mtime -i -o "%%(title)s.%%(ext)s" -x --audio-format vorbis --audio-quality 0 --ignore-config %URL%
+youtube-dl --newline --no-mtime -i -o "%%(title)s.%%(ext)s" -x --audio-format %FORMAT% --audio-quality 0 --ignore-config %URL%
 choice /n /c:yn /m "Another process [Y/N]"
 if errorlevel 2 goto opendir
 if errorlevel 1 goto sing
@@ -15,8 +18,19 @@ if not exist "list.txt" echo # Paste your URLs below with each URL on a line, th
 start %cd%\list.txt
 choice /n /c:yn /m "Continue [Y/N]"
 if errorlevel 2 exit
-if errorlevel 1 youtube-dl --newline --no-mtime -i -o "%%(title)s.%%(ext)s" -x --audio-format vorbis --audio-quality 0 --ignore-config -a list.txt
+if errorlevel 1 youtube-dl --newline --no-mtime -i -o "%%(title)s.%%(ext)s" -x --audio-format %FORMAT% --audio-quality 0 --ignore-config -a list.txt
 goto opendir
+
+:form
+echo CHOOSE A FORMAT.
+:form404
+echo FORMAT: aac, flac, mp3, m4a, opus, vorbis, or wav.
+set /p FORMAT=FORMAT:
+if "%FORMAT%" neq "aac" if "%FORMAT%" neq "flac" if "%FORMAT%" neq "mp3" if "%FORMAT%" neq "m4a" if "%FORMAT%" neq "opus" if "%FORMAT%" neq "vorbis" if "%FORMAT%" neq "wav" (echo INVALID FORMAT, TRY AGAIN
+goto form404)
+(echo # Here are saved the default conversion format, you can change it manually here.
+echo FORMAT=%FORMAT%) > config.txt
+goto start
 
 :opendir
 choice /n /c:yn /m "Open directory [Y/N]"
