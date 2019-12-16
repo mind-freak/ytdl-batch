8313d85613c258c28a0852aa7fc91b748f9d7276
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-12-16 16:47:51 -0300

    Update video-public.bat

--- ytdl-public/video-public.bat
+++ ytdl-public/video-public.bat
@@ -3,7 +3,7 @@
 set /p URL=URL:
 youtube-dl --ignore-config -F %URL%
 set /p FORMAT=FORMAT CODE:
-youtube-dl --no-mtime -i -o "%%(title)s.%%(ext)s" -f %FORMAT% --ignore-config %URL%
+youtube-dl --no-mtime -i -o "%%(title)s.%%(ext)s" -f %FORMAT% --ignore-config "%URL%"
 choice /n /c:yn /m "Another process [Y/N]"
 if errorlevel 2 goto opendir
 if errorlevel 1 goto vid
