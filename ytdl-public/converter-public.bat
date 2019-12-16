70d43b24f52c7973cc96af1bb0494014130da7a5
Mind freak <50534312+mind-freak@users.noreply.github.com>
2019-12-16 16:47:38 -0300

    Update converter-public.bat

--- ytdl-public/converter-public.bat
+++ ytdl-public/converter-public.bat
@@ -4,10 +4,15 @@ echo THIS FOLDER TO .OGG / VORBIS TO THE FOLDER "OGG";
 echo IF "OGG" DOESN'T EXIST, IT WILL BE CREATED;
 echo FFMPEG AND THE MUSIC FILES MUST BE WITH THE CONVERTER;
 echo CHOOSE THE FILE TYPE YOU WANNA CONVERT TO OGG.
-choice /n /c:123456 /m "MP3 [1] // FLAC [2] // WAV [3] // M4A [4] // Other [5] // Exit [6]"
-if errorlevel 6 exit
+if not exist "config-convert.txt" (echo # Here are saved the default conversion format, you can change it manually here.
+echo FORMAT=ogg) > config-convert.txt
+for /f "delims= skip=1" %%x in (config-convert.txt) do (set "%%x")
+:start
+choice /n /c:1234567 /m "MP3 [1] // FLAC [2] // WAV [3] // OGG [4] // Other [5] // Change Default Format [6] // Exit [7]"
+if errorlevel 7 exit
+if errorlevel 6 goto form
 if errorlevel 5 goto PROMPTothr
-if errorlevel 4 goto PROMPTm4a
+if errorlevel 4 goto PROMPTogg
 if errorlevel 3 goto PROMPTwav
 if errorlevel 2 goto PROMPTflac
 if errorlevel 1 goto PROMPTmp3
@@ -20,8 +25,8 @@ goto 404
 :PROMPTwav
 if exist "*.wav" goto convertwav
 goto 404
-:PROMPTm4a
-if exist "*.m4a" goto convertm4a
+:PROMPTogg
+if exist "*.ogg" goto convertogg
 goto 404
 :PROMPTothr
 set /p FORMAT=FORMAT (without the dot):
@@ -30,24 +35,36 @@ goto 404
 
 :convertothr
 if not exist "OGG" mkdir OGG
-for %%a in ("*.%FORMAT%") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+for %%a in ("*.%FORMAT%") do ffmpeg -i "%%a" "OGG/%%~na.%FMT%"
 goto opendir
-:convertm4a
+:convertogg
 if not exist "OGG" mkdir OGG
-for %%a in ("*.m4a") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+for %%a in ("*.ogg") do ffmpeg -i "%%a" "OGG/%%~na.%FMT%"
 goto opendir
 :convertwav
 if not exist "OGG" mkdir OGG
-for %%a in ("*.wav") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+for %%a in ("*.wav") do ffmpeg -i "%%a" "OGG/%%~na.%FMT%"
 goto opendir
 :convertmp3
 if not exist "OGG" mkdir OGG
-for %%a in ("*.mp3") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+for %%a in ("*.mp3") do ffmpeg -i "%%a" "OGG/%%~na.%FMT%"
 goto opendir
 :convertflac
 if not exist "OGG" mkdir OGG
-for %%a in ("*.flac") do ffmpeg -i "%%a" -c:a libvorbis "OGG/%%~na.ogg"
+for %%a in ("*.flac") do ffmpeg -i "%%a" "OGG/%%~na.%FMT%"
 goto opendir
+
+:form
+echo -- Change Default Format.
+:form404
+echo FORMATS AVAILABLE: flac, mp3, ogg, or wav.
+set /p FMT=FORMAT:
+if "%FMT%" neq "flac" if "%FMT%" neq "mp3" if "%FMT%" neq "ogg" if "%FMT%" neq "wav" (echo INVALID FORMAT, TRY AGAIN
+goto form404)
+(echo # Here are saved the default conversion format, you can change it manually here.
+echo FMT=%FMT%) > config-convert.txt
+echo DEFAULT FORMAT CHANGED SUCCESSFULY
+goto start
 
 :opendir
 choice /n /c:yn /m "Open OGG directory [Y/N]"
